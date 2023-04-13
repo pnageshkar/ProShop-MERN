ab87e84e7207fe366e7c32e141b66c9ff24525e4
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2023-04-13 18:17:34 +0530

    Checked in the basic layout

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import { Container,Row,Col } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            ProShop &copy; 2023
+          </Col>
+        </Row>
+      </Container>
+      </footer>
+  )
+}
+export default Footer
